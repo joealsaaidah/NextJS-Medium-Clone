10e610f1d56802af4b2fb41dc96e0c2d3d3323f1
joealsaaidah <alsaaidahyousef@gmail.com>
2022-01-20 12:11:34 +0800

    set up the project files

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className=''>
+      <Head>
+        <title>Medium-Clone</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <h1>Homepage</h1>
+    </div>
+  );
+}
